237e53f1835a3a4cee8a0219fc91d99ad8b808b8
Anna_Avetisyan <annamariavetis@gmail.com>
2019-11-05 09:40:35 +0400

    second commit

--- src/main/Main.java
+++ src/main/Main.java
@@ -2,6 +2,12 @@ package main;
 
 public class Main {
 
+    private String name;
+
+    public String getSomeName() {
+        return this.name;
+    }
+
     public static void main(String[] args) {
         System.out.println("Hello World!");
     }
